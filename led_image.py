c5e9b0edc70bfe9f23289848815f82de6e60dbc3
jngeun <mine1372@korea.ac.kr>
2021-05-17 17:46:52 +0900

    add ui

--- led_image.py
+++ led_image.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 CAM_ID = 0
-
+LED_WIDTH = 10
 def capture(camid=CAM_ID):
     cam = cv2.VideoCapture(camid)
     if cam.isOpened() == False:
@@ -22,19 +22,29 @@ if __name__ == "__main__":
     h,w,c = cam_img.shape
     # upper_led = cv2.imread('data/images/upper_led.jpg',cv2.IMREAD_COLOR)
 
-    rampl = (np.linspace(1, 0, 30) * 255).astype(np.uint8)
+    rampl = (np.linspace(1, 0, LED_WIDTH) * 255).astype(np.uint8)
     rampl = np.tile(np.transpose(rampl), (480, 1))
     rampl = cv2.merge([rampl, rampl, rampl])
-    up = cv2.rotate(rampl,cv2.ROTATE_180)
-    cv2.imwrite('data/images/led_right.jpg',up)
+    cv2.imwrite('data/images/led_left.jpg', rampl)
+    rampr = cv2.rotate(rampl,cv2.ROTATE_180)
+    cv2.imwrite('data/images/led_right.jpg', rampr)
 
-    roi = cam_img[0:30,0:640]
+    rampu = (np.linspace(1, 0, LED_WIDTH) * 255).astype(np.uint8)
+    rampu = np.tile(np.transpose(rampu), (640, 1))
+    rampu = cv2.merge([rampu, rampu, rampu])
+    rampu = cv2.rotate(rampu, cv2.ROTATE_90_CLOCKWISE)
+    cv2.imwrite('data/images/led_up.jpg',rampu)
+    rampd = cv2.rotate(rampu, cv2.ROTATE_180)
+    cv2.imwrite('data/images/led_down.jpg',rampd)
 
-    dst = cv2.add(roi,up)
-    cam_img[0:30, 0:640] = dst
 
-    cv2.imshow('cam_img', cam_img)
-    cv2.imshow('-',up)
+
+    # roi = cam_img[0:30,0:640]
+    #
+    # dst = cv2.add(roi,up)
+    # cam_img[0:30, 0:640] = dst
+
+    cv2.imshow('cam_img', rampl)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
